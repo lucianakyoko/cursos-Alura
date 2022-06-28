4364b88d7786d65abd2925be8d4811c0287ff4b4
Luciana Kyoko <69259218+lucianakyoko@users.noreply.github.com>
2022-06-28 14:48:30 -0300

    feat(livraria-alura): class 02 - Trabalhando com o Express

--- livraria-alura/server.js
+++ livraria-alura/server.js
@@ -1,17 +1,6 @@
-const http = require('http');
-const port = 3000;
+import app from './src/app.js';
+const port = process.env.PORT || 3000;
 
-const rotas = {
-  '/': 'Curso de Node',
-  '/livros': 'Entrei na página de livros',
-  '/autores': 'Listagem de autores',
-  '/editora': 'Pagina de editora',
-  '/sobre': 'Info sobre o projeto'
-};
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, {'content-Type': 'text/plain'});
-  res.end(rotas[req.url]);
-});
 
-server.listen(port, () => console.log(`Servidor escutando em http://localhost:${port}`));
+app.listen(port, () => console.log(`Servidor escutando em http://localhost:${port}`));
