07f26b2789dc5dcb97e250db4a10ada8d6e2a85d
Luciana Kyoko <69259218+lucianakyoko@users.noreply.github.com>
2022-07-05 19:47:12 -0300

    feat(escola-ingles): class 01 - Apresentando o GraphQL

---
+++ escola-ingles/api/data/index.js
@@ -0,0 +1,22 @@
+const { ApolloServer, gql } = require('apollo-server');
+
+const users = [
+  {
+    nome: 'Ana',
+    ativo: true
+  },
+  {
+    nome: 'Marcia',
+    ativo: false
+  }
+];
+
+const typeDefs = gql `
+  type User {
+    nome: String!
+    ativo: Boolean!
+    email: String
+  }
+`
+
+const server = new ApolloServer({ typeDefs, resolvers });
