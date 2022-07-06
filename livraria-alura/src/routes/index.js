4513c6e58c3a36de677fcfbfe6d2d3c541c25f74
Luciana Kyoko <69259218+lucianakyoko@users.noreply.github.com>
2022-07-06 18:43:58 -0300

    feat(livraria-alura): class 05 - Aprimorando o projeto

--- livraria-alura/src/routes/index.js
+++ livraria-alura/src/routes/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import livros from './livrosRoutes.js';
+import autores from './autoresRoutes.js';
 
 const routes = (app) => {
   app.route('/').get((req, res) => {
@@ -8,7 +9,9 @@ const routes = (app) => {
 
   app.use(
     express.json(),
-    livros
+    livros,
+    autores
+
   )
 }
 
