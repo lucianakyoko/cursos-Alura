bfb42288668e3c0cd1d7a9eaca72419cf5063c34
Luciana Kyoko <69259218+lucianakyoko@users.noreply.github.com>
2022-07-10 13:23:12 -0300

    feat(escola-ingles): class 04 - Mutations

--- escola-ingles/api/user/resolvers/userResolver.js
+++ escola-ingles/api/user/resolvers/userResolver.js
@@ -2,6 +2,11 @@ const userResolvers = {
   Query: {
     users: (root, args, { dataSources }) => dataSources.usersAPI.getUsers(),
     user: (root, { id}, { dataSources }) => dataSources.usersAPI.getUSerById(id)
+  },
+  Mutation: {
+    adicionaUser: async (root, user, { dataSources }) => dataSources.usersAPI.adicionaUser(user),
+    atualizaUser: async (root, novosDados, { dataSources }) => dataSources.usersAPI.atualizaUser(novosDados),
+    deletaUser: async (root, { id }, { dataSources }) => dataSources.usersAPI.deletaUser(id)
   }
 };
 
