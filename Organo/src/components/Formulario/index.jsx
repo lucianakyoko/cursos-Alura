031a928f982cdf36a0dbb0c42b78cdbfb8d8510f
Luciana Kyoko <69259218+lucianakyoko@users.noreply.github.com>
2022-07-13 09:15:55 -0300

    feat(Organo): class 02 - Trabalhando com props

--- Organo/src/components/Formulario/index.jsx
+++ Organo/src/components/Formulario/index.jsx
@@ -1,14 +1,33 @@
 import './Formulario.css';
 import CampoTexto from '../CampoTexto';
+import ListaSuspensa from '../ListaSuspensa';
+import Botao from '../Botao';
 
 const Formulario = () => {
+  const times = [
+    'Programação',
+    'Front-End',
+    'Data Science',
+    'DevOps',
+    'UX e Design',
+    'Mobile',
+    'Inovação e Gestão'
+  ];
+
+  const aoSalvar = event => {
+    event.preventDefault();
+    console.log('Form foi submetido')
+  }
+
   return (
     <section className='formulario'>
-      <form >
+      <form onSubmit={aoSalvar}>
         <h2>Preencha os dados para criar o card do colaborador</h2>
-        <CampoTexto label="Nome" placeholder="Digite o seu nome"/>
-        <CampoTexto label="Cargo" placeholder="Digite o seu cargo"/>
+        <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite o seu nome"/>
+        <CampoTexto obrigatorio={true} label="Cargo" placeholder="Digite o seu cargo"/>
         <CampoTexto label="Imagem" placeholder="Digite o endenreço da imagem"/>
+        <ListaSuspensa obrigatorio={true} itens={times} label="Time"/>
+        <Botao>Criar Card</Botao>
       </form>
     </section>
   );
